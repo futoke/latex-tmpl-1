d84b684748560535203b8e9cd0afae74b66451fc
Евгений Привалов <saqel@yandex.ru>
2015-04-19 02:10:18 +0300

    Update interface.py

--- GUI/interface.py
+++ GUI/interface.py
@@ -3,6 +3,8 @@ import sys
 
 from gi.repository import Gtk, Gdk
 
+from GUI.bachelors import BachelorsWindow
+
 class CreateMainWindow:
 
 	GLADEFILE = "open-main.glade"
@@ -11,10 +13,23 @@ class CreateMainWindow:
 		builder = Gtk.Builder()
 		builder.add_from_file(self.GLADEFILE)
 		builder.connect_signals(Handlers())
+		global window
+		window = builder.get_object("window1")
+		window.show_all()
 		Gtk.main()
 	
 
 class Handlers():
-  
-  def for_future(self):
-    pass
+
+	def open_bachelors_blanks(self, button):
+		BachelorsWindow()
+		window.destroy()
+
+	def open_master_blanks(self, button):
+		window.destroy()
+		pass
+
+	def open_specialist_blanks(self, button):
+		window.destroy()
+		pass
+
