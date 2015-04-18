bb1e4e3824ce4122c80dec132e5c8154ef7c7353
Евгений Привалов <saqel@yandex.ru>
2015-04-19 02:02:54 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,35 @@
 import os
+import sys
 
-from GUI.interface import CreateMainWindow
+from gi.repository import Gtk, Gdk
 
+from GUI.bachelors import BachelorsWindow
 
-if __name__ == "__main__":
-	CreateMainWindow()
+class CreateMainWindow:
+
+	GLADEFILE = "open-main.glade"
+	def __init__(self):
+
+		builder = Gtk.Builder()
+		builder.add_from_file(self.GLADEFILE)
+		builder.connect_signals(Handlers())
+		global window
+		window = builder.get_object("window1")
+		window.show_all()
+		Gtk.main()
+	
+
+class Handlers():
+
+	def open_bachelors_blanks(self, button):
+		BachelorsWindow()
+		window.destroy()
+
+	def open_master_blanks(self, button):
+		window.destroy()
+		pass
+
+	def open_specialist_blanks(self, button):
+		window.destroy()
+		pass
+
